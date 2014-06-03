241d63d77623e48e857952855960c1fd6b0ee767
Adam Normoyle <normoyle93@gmail.com>
2014-06-03 18:19:06 +0100

    first commit

---
+++ t1/Assets/CharacterControlls.js
@@ -0,0 +1,64 @@
+﻿#pragma strict
+
+var MoveUp : KeyCode;
+var MoveDown : KeyCode;
+var MoveLeft : KeyCode;
+var MoveRight : KeyCode;
+var Speed : float = 10;
+var playeranim : Animator;
+function Start() {
+
+ 
+playeranim = GetComponentInChildren(Animator);
+
+}
+
+
+function Update () {
+
+if (Input.GetKey(MoveUp)){
+
+
+playeranim.SetBool("up",true);
+playeranim.SetBool("down",false);
+playeranim.SetBool("left",false);
+playeranim.SetBool("right",false);
+rigidbody.velocity.z = Speed * Time.deltaTime;
+
+
+
+}
+else if (Input.GetKey(MoveDown)){
+playeranim.SetBool("down",true);
+playeranim.SetBool("up",false);
+playeranim.SetBool("left",false);
+playeranim.SetBool("right",false);
+rigidbody.velocity.magnitude.z = Speed * Time.deltaTime;
+
+}
+else if (Input.GetKey(MoveLeft)){
+playeranim.SetBool("left",true);
+playeranim.SetBool("down",false);
+playeranim.SetBool("up",false);
+playeranim.SetBool("right",false);
+rigidbody.velocity.x = Speed * Time.deltaTime;
+
+}
+else if (Input.GetKey(MoveRight)){
+playeranim.SetBool("right",true);
+playeranim.SetBool("down",false);
+playeranim.SetBool("left",false);
+playeranim.SetBool("up",false);
+rigidbody.velocity.x = Speed * Time.deltaTime;
+}
+
+else{
+rigidbody.velocity.z = 0;
+rigidbody.velocity.x = 0;
+playeranim.SetBool("up",false);
+playeranim.SetBool("down",false);
+playeranim.SetBool("left",false);
+playeranim.SetBool("right",false);
+}
+
+}
---
+++ t1/Assets/HealthPickup.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+var healthvalue : int;
+
+function OnTriggerEnter(object: Collider){
+
+Debug.Log("health pickup!!!!");
+
+	if (object.gameObject.FindGameObjectWithTag("Player")){
+	Destroy(this.gameObject);
+	GameObject.Find("HealthBar").SendMessage ("ApplyHealthPickup", healthvalue);
+	}
+
+}
